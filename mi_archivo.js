81bdc3a150720b1efec01ed904e72bdefcd54e45
Angelo Escalante <aescalantev@ucenfotec.ac.cr>
2025-11-18 17:34:01 -0600

    Update mi_archivo.js

--- mi_archivo.js
+++ mi_archivo.js
@@ -1,7 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, mundo desde Java!");
-        saludar("nombre");
+        saludar("Angelo");
         sumar(5, 3);
     }
 
